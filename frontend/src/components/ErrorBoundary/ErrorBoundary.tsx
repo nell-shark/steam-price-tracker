6b61d36d841ef3ac604720ea639885570d36e646
nell-shark <krivolapov_vladislav@outlook.com>
2024-01-11 23:56:43 +0600

    Create ErrorBoundary.tsx

---
+++ frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  children?: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  public state: ErrorBoundaryState = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) return <h1>Something went wrong.</h1>;
+
+    return this.props.children;
+  }
+}
