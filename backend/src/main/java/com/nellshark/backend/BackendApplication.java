b1a305958dde1e1a5f6bd66c675d073165728901
nell-shark <krivolapov_vladislav@outlook.com>
2024-03-28 01:57:06 +0600

    Create GitHub actions

--- backend/src/main/java/com/nellshark/backend/BackendApplication.java
+++ backend/src/main/java/com/nellshark/backend/BackendApplication.java
@@ -1,26 +1,12 @@
 package com.nellshark.backend;
 
-import com.nellshark.backend.models.User;
-import com.nellshark.backend.models.UserRole;
-import com.nellshark.backend.services.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner commandLineRunner(UserService userService) {
-    return args -> {
-      User user = new User("admin@gmail.com", "password123");
-      user.setRole(UserRole.ROLE_ADMIN);
-      userService.createNewUser(user);
-    };
   }
 }
