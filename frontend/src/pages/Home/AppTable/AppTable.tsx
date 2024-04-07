077c30b4df857d0ce483dfc737a1cc730ca2c9c1
nell-shark <krivolapov_vladislav@outlook.com>
2024-04-07 14:39:41 +0600

    front: useState for status of AppTable.tsx

--- frontend/src/pages/Home/AppTable/AppTable.tsx
+++ frontend/src/pages/Home/AppTable/AppTable.tsx
@@ -10,42 +10,57 @@ import styles from "./AppTable.module.css";
 
 export function AppTable() {
   const [activePage, setActivePage] = useState<number>(1);
-  const [appsByPage, setAppsByPage] = useState<AppsByPage>();
+  const [appsByPage, setAppsByPage] = useState<AppsByPage | null>(null);
+  const [status, setStatus] = useState<"loading" | "completed" | "error">("loading");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
     async function fetchGamesByPage(page: number) {
-      const res = await appService.getAppsByPage(page);
-      setAppsByPage(res);
+      setStatus(() => "loading");
+      try {
+        const res = await appService.getAppsByPage(page);
+        setAppsByPage(res);
+        setStatus(() => "completed");
+      } catch (error) {
+        setStatus(() => "error");
+        setErrorMessage(() => (error as Error).message);
+      }
     }
     fetchGamesByPage(activePage);
   }, [activePage]);
 
   return (
-    <div className="text-center">
-      <Table hover className={`overflow-hidden rounded ${styles.appTable}`}>
-        <thead className="table-dark">
-          <tr>
-            <th>id</th>
-            <th>Name</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {appsByPage?.content.map(app => (
-            <AppItem key={app.id} id={app.id} name={app.name} imageUrl={app.imageUrl} />
-          ))}
-        </tbody>
-      </Table>
-      {appsByPage && (
-        <PaginationControl
-          page={activePage}
-          between={4}
-          total={appsByPage.totalElements}
-          limit={appsByPage.size}
-          changePage={page => setActivePage(() => page)}
-          ellipsis={1}
-        />
+    <>
+      {status === "loading" && <h1>Loading...</h1>}
+      {status === "error" && <h1>{errorMessage}</h1>}
+      {status === "completed" && (
+        <div className="text-center">
+          <Table hover className={`overflow-hidden rounded ${styles.appTable}`}>
+            <thead className="table-dark">
+              <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Image</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appsByPage?.content.map(app => (
+                <AppItem key={app.id} id={app.id} name={app.name} imageUrl={app.imageUrl} />
+              ))}
+            </tbody>
+          </Table>
+          {appsByPage && (
+            <PaginationControl
+              page={activePage}
+              between={4}
+              total={appsByPage.totalElements}
+              limit={appsByPage.size}
+              changePage={page => setActivePage(() => page)}
+              ellipsis={1}
+            />
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }
