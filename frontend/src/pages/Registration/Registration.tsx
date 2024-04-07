f256403a00272a8fbbe25adb6859080d9fc80bcc
nell-shark <krivolapov_vladislav@outlook.com>
2024-04-07 19:56:24 +0600

    front: disable login/registration button for invalid inputs

--- frontend/src/pages/Registration/Registration.tsx
+++ frontend/src/pages/Registration/Registration.tsx
@@ -1,43 +1,90 @@
-import { useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import ReCAPTCHA from "react-google-recaptcha";
 import { Link } from "react-router-dom";
+
+import { userService } from "@/services/userService";
+import { User } from "@/types/user";
 
 import styles from "./Registration.module.css";
 
 export function Registration() {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const captchaRef = useRef(null);
+  const captchaRef = useRef<ReCAPTCHA>(null);
 
-  function handleClick(event: React.MouseEvent<HTMLElement>) {
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(false);
+  const [isCaptchaValid, setIsCaptchaValid] = useState<boolean>(false);
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(emailRef.current?.value);
-    console.log(passwordRef.current?.value);
+    if (!isCaptchaValid || !isEmailValid || !isPasswordValid) {
+      return;
+    }
+    const user: User = {
+      email: emailRef.current!.value,
+      password: passwordRef.current!.value
+    };
+    const captcha = captchaRef.current!.getValue()!;
+    await userService.postUser(user, captcha);
+  }
+
+  function isValidEmail(email: string) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  function handleEmailChange() {
+    setIsEmailValid(() => isValidEmail(emailRef.current?.value ?? ""));
+  }
+
+  function handlePasswordChange() {
+    setIsPasswordValid(() => (passwordRef.current?.value.length ?? 0) >= 8);
+  }
+
+  function handleCaptchaChange(value: string | null) {
+    setIsCaptchaValid(() => value !== null);
   }
 
   return (
     <div className={`d-flex align-items-center justify-content-center ${styles.register}`}>
-      <Form method="POST" className={styles.form}>
+      <Form method="POST" className={styles.form} onSubmit={e => handleSubmit(e)}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" ref={emailRef} required />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={() => handleEmailChange()}
+            ref={emailRef}
+            required
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword" className="mt-4">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" ref={passwordRef} required />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={() => handlePasswordChange()}
+            ref={passwordRef}
+            required
+          />
         </Form.Group>
 
         <div className="mt-3 d-flex align-items-center justify-content-center ">
-          <ReCAPTCHA sitekey={import.meta.env.VITE_CAPTCHA_SITE_KEY} ref={captchaRef} />
+          <ReCAPTCHA
+            sitekey={import.meta.env.VITE_CAPTCHA_SITE_KEY}
+            ref={captchaRef}
+            onChange={e => handleCaptchaChange(e)}
+          />
         </div>
 
         <Button
           className="mt-4 w-100"
           variant="primary"
           type="submit"
-          onClick={e => handleClick(e)}
+          disabled={!isEmailValid || !isPasswordValid || !captchaRef.current?.getValue()}
         >
           Register
         </Button>
